cfa6b54a3afd4ff74c7c4d7daff472dd44c07f64
ahmed <ahmxd989@gmail.com>
2024-09-29 17:18:28 +0400

    updated app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { WalletProvider, ConnectionProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+  <ConnectionProvider endpoint="https://eth-mainnet.g.alchemy.com/v2/IbA-eashNtxQ2fo250TIj5e3-0byxlLf/getNFTs/?owner=vitalik.eth">
+    <WalletProvider wallets={[]} >
+      <WalletModalProvider>
+        
+    return <Component {...pageProps} />;
+
+      </WalletModalProvider>
+    </WalletProvider>
+  </ConnectionProvider>
 }
