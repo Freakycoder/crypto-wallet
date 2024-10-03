d06f676b4fd2b97b9c043d70ae791917da228706
ahmed <ahmxd989@gmail.com>
2024-10-03 11:14:49 +0400

    updated app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,29 @@
+import "@solana/wallet-adapter-react-ui/styles.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { WalletProvider, ConnectionProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { useMemo } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { clusterApiUrl } from "@solana/web3.js";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Specify the network (mainnet, testnet, devnet)
+  const network = WalletAdapterNetwork.Devnet;
+  
+  // You can also provide a custom RPC endpoint
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  // Initialize the wallets you want to support
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter({ network })],
+    [network]
+  );
+
   return (
-    <ConnectionProvider endpoint="https://api.mainnet-beta.solana.com">
-      <WalletProvider wallets={[]}>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>
           <Component {...pageProps} />
         </WalletModalProvider>
